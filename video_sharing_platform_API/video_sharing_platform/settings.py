9c5caebda6261e4e2333eb8f686ee83b6f2ae0ff
Nguyen Le Ngoc Huy <71955356+ngochuy2001-neo@users.noreply.github.com>
2025-07-12 23:14:44 +0700

    Upload to database

--- video_sharing_platform_API/video_sharing_platform/settings.py
+++ video_sharing_platform_API/video_sharing_platform/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'media'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'video_sharing_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'video_sharing_platform_db',            
+        'USER': 'postgres',          
+        'PASSWORD': '2001',  
+        'HOST': 'localhost',       
+        'PORT': '5432',            
     }
 }
 
