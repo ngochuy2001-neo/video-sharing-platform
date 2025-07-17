22595140bc66f13aa530a7ea237db8b8d3bcedaf
Nguyen Le Ngoc Huy <71955356+ngochuy2001-neo@users.noreply.github.com>
2025-07-17 21:34:13 +0700

    Update API

--- video_sharing_platform_UI/utils/axios.ts
+++ video_sharing_platform_UI/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://http://192.168.10.83/",
   // withCredentials: true, // nếu cần gửi cookie
 });
 
